de0cb5cef50d50275810ea99f1070282f9c3354d
Bee-Yang <yang737@uwm.edu>
2019-04-16 13:36:51 -0500

    template for views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class User(View):
+    def get(self, request):
+        some_data = {"my_text": "hello world"}
+        return render(request, 'main/users.html', some_data)
