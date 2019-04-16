1f70a3f9be3c71bfa37c49839ea6b6d2757f1ada
marsole4 <bjmarsolek@gmail.com>
2019-04-16 13:38:52 -0500

    made gift and user models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+
+class Gift(models.Model):
+    name = models.CharField(max_length=20)
+    receiver = models.ForeignKey(User, on_delete=models.CASADE, blank=False, null=True, default=None)
+
